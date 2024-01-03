46e2ccc869995d005191af19d481540b597d358b
Victor Meneses <victor.pinho@aluno.uepb.edu.br>
2024-01-03 08:07:11 -0300

    template basis

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import MyButton from './MyButton'
+import MyDiv from './MyDiv'
+
+function App() {
+  
+  return (
+    <>
+      <MyButton/>
+      <MyDiv/>
+    </>
+  )
+}
+
+export default App
