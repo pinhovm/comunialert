a8746a6b914c48e335d84a041b1c647dd4074058
Victor Meneses <victor.pinho@aluno.uepb.edu.br>
2024-03-25 02:17:45 -0300

    Atualizacao Login page e adicao Navbar

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-// import MyDiv from "./MyDiv";
-// <MyDiv />
-function App() {
-  return <div className="app bg-sky-500 z-10">{}</div>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import Login from "./Login";
+import Navbar from "./Navbar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Login />
+    <Navbar />
+  </React.StrictMode>
 );
